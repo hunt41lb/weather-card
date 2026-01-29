311a32e0e8da629bf02e74c808ae3522fab3795b
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2026-01-29 09:12:17 -0500

    Add comments to effect styles index file

--- src/styles/effects/index.ts
+++ src/styles/effects/index.ts
@@ -9,4 +9,5 @@ export { snowEffectStyles } from './snow.styles';
 export { sunnyEffectStyles } from './sunny.styles';
 export { cloudyEffectStyles } from './cloudy.styles';
 export { rainEffectStyles } from './rain.styles';
+
 export { fogEffectStyles } from './fog.styles';
